3a7efb6c420ae9915c051cb35b6a4f63b2d1f856
Venelin Nikolov <venelinn@gmail.com>
2019-04-02 06:05:25 -0400

    keep trying

--- src/sections/portfolio/index.js
+++ src/sections/portfolio/index.js
@@ -1,39 +1,81 @@
 import React from "react"
-import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 import Section from "../../components/Section";
 //import ImageGrid from "../../components/ImageGrid";
 
 import "./portfolio.scss"
 
-const Portfolio = ({ data }) => (
-  <Section
-    className="portfolio"
-    title="Portfolio"
-		description="Check Out Some of My Works"
-    >
+const Portfolio = () => (
+  <StaticQuery
+    query={graphql`
+      query PortfolioQuery {
+        allPortfolioJson {
+          edges {
+            node {
+              name
+              url
+              types
+            }
+          }
+        }
+      }
+    `}
+    render={ data => (
+      <Section
+        className="portfolio"
+        title="Portfolio"
+        description="Check Out Some of My Works"
+        >
+      <div>
+        {/* {data.allPortfolioJson.edges.map( node => (
+         <div>{node.name}</div>
+        ))} */}
+        <ul className="about__info-list">asdasdas</ul>
+      </div>
+      </Section>
+    )}
+  />
+)
 
-    <div className="portfolio-content">
-      {data.allPortfolioJson.edges.map( node => (
-        <div>{node.name}</div>
-      ))}
-    </div>
-  </Section>
-);
-
+console.log(Portfolio);
 
 export default Portfolio
 
-export const query = graphql`
-  query projectsQuery {
-    allPortfolioJson {
-      edges {
-        node {
-          name
-          url
-          types
-        }
-      }
-    }
-  }
-`
+// const IndexPortfolio = ({ data, props }) => (
+//   <Section
+//     className="portfolio"
+//     title="Portfolio"
+// 		description="Check Out Some of My Works"
+//     >
+
+//     <div className="portfolio-content">
+//       {data.props.allPortfolioJson.edges.map( node => (
+//         <div>{node.name}</div>
+//       ))}
+//     </div>
+//   </Section>
+// );
+
+// export default PortfolioIndex
+
+// class IndexPortfolio extends React.Component {
+//   render() {
+//     //console.log(this.data);
+//     return (
+//       <Section
+//         className="portfolio"
+//         title="Portfolio"
+//         description="Check Out Some of My Works"
+//         >
+
+//           {/* {sections.map(({ node: section }) => (
+//             <div key={section.id} name={section.name}>
+//               <div key={section.id} />
+//             </div>
+//           ))} */}
+//       </Section>
+//     )
+//   }
+// }
+
+
