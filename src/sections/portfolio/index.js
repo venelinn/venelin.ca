823cf3cedc5ec5bfafd4559006bee8657153ee5b
Venelin Nikolov <venelinn@gmail.com>
2019-04-03 06:52:13 -0400

    keep trying

--- src/sections/portfolio/index.js
+++ src/sections/portfolio/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { StaticQuery, graphql } from 'gatsby'
-import Section from "../../components/Section";
+import Section from "../../components/Section"
+import Img from "gatsby-image"
 
 import "./portfolio.scss"
 
@@ -8,29 +9,51 @@ const PortfolioList = () => (
   <StaticQuery
   query={graphql`
     query ProjectsQuery {
-      allProjectsJson {
+      allPortfolioJson {
         edges {
           node {
+            id
             name
             url
             types
+            description
+            photos {
+              childImageSharp {
+                resolutions(
+                  width: 256
+                  height: 256
+                  cropFocus: CENTER
+                  quality: 92
+                ) {
+                  src
+                }
+              }
+            }
           }
         }
       }
     }
   `}
     render={data => (
-      <>
-        <ul>
+      <div className="portfolio-content">
+        <div className="stack">
          {
-          data.allProjectsJson.edges.map(item => (
-            <li key={item.node.name}>
-              {item.node.name}
-            </li>
+          data.allPortfolioJson.edges.map(item => (
+            <div className="bgrid folio-item" key={item.node.id}>
+              <a href="#" className="folio-item__link">
+                <Img resolutions={item.node.childImageSharp.resolutions.src} className={styles.image} />
+                <span className="folio-item-table">
+                  <span className="folio-item-cell">
+                      <h3 className="folio-title">{item.node.name}</h3>
+                      <span className="folio-types">{item.node.types}</span>
+                  </span>
+                </span>
+              </a>
+          </div>
           ))
         }
-        </ul>
-      </>
+        </div>
+      </div>
     )}
   />
 )
